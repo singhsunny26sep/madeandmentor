a047514b3b0f1bfd33c8bdcadf2ef88d27f6a17f
singhsunny26sep <101707682+singhsunny26sep@users.noreply.github.com>
2026-03-20 12:48:01 +0530

    new

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -55,7 +55,10 @@ export default function Home() {
             {/* Video Container */}
             <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
            
-              <iframe className="absolute top-0 left-0 w-full h-full rounded-2xl shadow-2xl" width="560" height="315" src="https://www.youtube.com/embed/71uoP6i-BtE?si=UoZSI_xHB-0nUWeI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+              
+            
+            <iframe className="absolute top-0 left-0 w-full h-full rounded-2xl shadow-2xl" width="560" height="315" src="https://www.youtube.com/embed/71uoP6i-BtE?si=oLDAnYa_x9D5ZO1Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            
             </div>
           </div>
         </div>
