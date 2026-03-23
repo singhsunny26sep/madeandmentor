0c17932e26dfb0da4c4172253596926a44483ac3
singhsunny26sep <101707682+singhsunny26sep@users.noreply.github.com>
2026-03-23 21:54:32 +0530

    newchanges

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import ReactFacebookPixel from 'react-facebook-pixel';
+
+// Facebook Pixel Configuration
+const pixelId = '4514046808881979';
+ReactFacebookPixel.init(pixelId, {}, { debug: false, autoConfig: true });
+ReactFacebookPixel.pageView();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
